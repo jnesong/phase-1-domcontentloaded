c03ef0f651a87828d8f1e3516b1eb63edf6e04bd
Jenny Chau <jnechau@gmail.com>
2021-11-07 00:31:07 -0400

    commit tree f36f42e2d27c9c7a0c83a5189b3495492e7fe79d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    const updateText = document.querySelector("#text");
+    updateText.textContent = "This is really cool!";
+    console.log("After DOM loaded");
+
+
+
+
+
+})
+
+console.log("Before DOM loaded")
